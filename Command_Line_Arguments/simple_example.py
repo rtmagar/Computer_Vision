d368453ef9247363a12e91623f0db9da5fcc3442
rtmagar <rahulthapa123@outlook.com>
2021-01-23 14:56:29 +0545

    First Commit

---
+++ Command_Line_Arguments/simple_example.py
@@ -0,0 +1,12 @@
+# import the necessary packages
+import argparse
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-n", "--name", required=True,
+	help="name of the user")
+args = vars(ap.parse_args())
+print(args)
+# display a friendly message to the user
+print("Hi there {}, it's nice to meet you!".format(args["name"]))
+# print(f"Hi there", {args['name']},", it's nice to meet you")
+
