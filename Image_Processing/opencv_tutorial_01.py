8ed10e63701fae7c71b8c69d9dfdbc6415f99b86
rtmagar <rahulthapa123@outlook.com>
2021-01-26 14:05:30 +0545

    rotate, draw_shape, putText

--- Image_Processing/opencv_tutorial_01.py
+++ Image_Processing/opencv_tutorial_01.py
@@ -44,6 +44,63 @@ resized = imutils.resize(image, width=300)
 cv2.imshow("Image", resized)
 cv2.waitKey(0)
 
+# let's rotate an image 45 degrees clockwise using OpenCV by first
+# computing the image center, then constructing the rotation matrix,
+# and then finally applying the affine warp
+center = (w // 2, h // 2)
+M = cv2.getRotationMatrix2D(center, -45, 1.0)
+rotated = cv2.warpAffine(image, M, (w, h))
+cv2.imshow("OpenCV Rotation", rotated)
+cv2.waitKey(0)
+
+rotated = imutils.rotate(image, -45)
+cv2.imshow("Imutils Rotation", rotated)
+cv2.waitKey(0)
+
+# OpenCV doesn't "care" if our rotated image is clipped after rotation
+# so we can instead use another imutils convenience function to help
+# us out
+rotated = imutils.rotate_bound(image, 45)
+cv2.imshow("Imutils Bound Rotation", rotated)
+cv2.waitKey(0)
+
+# apply a Gaussian blur with a 11*11 kernel to the image to smooth it,
+# useful when reducing high frequency noise
+blurred = cv2.GaussianBlur(image, (9, 9), 0)
+cv2.imshow("Blurred", blurred)
+cv2.waitKey(0)
+
+# draw a 2px thick red rectangle surrounding the face
+output = image.copy()
+cv2.rectangle(output, (320, 60), (420, 160), (0, 0, 255), 2)
+cv2.imshow("Rectangle", output)
+cv2.waitKey(0)
+
+# draw a blue 20px (filled in) circle on the image centered at
+# x=300, y=150
+output = image.copy()
+cv2.circle(output, (400, 150), 20, (255, 0, 0), -1)
+cv2.imshow("Circle", output)
+cv2.waitKey(0)
+
+# draw a 5px thick red line from x=60,y=20 to x=400,y=200
+output = image.copy()
+cv2.line(output, (60, 20), (400, 200), (0, 0, 255), 5)
+cv2.imshow("Line", output)
+cv2.waitKey(0)
+
+# draw green text on the image
+output = image.copy()
+cv2.putText(output, "OpenCV + Jurassic Park!!!", (10, 25),
+            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 2), 2)
+cv2.imshow("Text", output)
+cv2.waitKey(0)
+
+
+
+
+
+
 
 
 
