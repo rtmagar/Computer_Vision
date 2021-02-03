33100d4d7a48aba37bb4cf4c44dd4743f512b085
rtmagar <rahulthapa123@outlook.com>
2021-02-03 20:05:02 +0545

    Build_Train_Simple_Model

--- Deep_Learning/train_simple_nn.py
+++ Deep_Learning/train_simple_nn.py
@@ -58,5 +58,56 @@ for imagePath in imagePaths:
     data = np.array(data, dtype='float') / 255.0
     labels = np.array(labels)
 
+# partition the data into training and testing splits using 75% of
+# the data for training and the remaining 25% for testing
+(trainX, testX, trainY, testY) = train_test_split(data,
+                                    labels, test_size=0.25, random_state=42)
 
+# convert the labels from integers to vectors (for 2-class, binary
+# classification you should use keras' to_categorical function
+# instead as the scikit_learn's LabelBinarizer will not return a vector)
+lb = LabelBinarizer()
+trainY = lb.fit_transform(trainY)
+testY = lb.transform(testY)
+
+# define the 3072-1024-512-3 architecture using Keras
+model = Sequential()
+model.add(Dense(1024, input_shape=(3072,), activation='sigmoid'))
+model.add(Dense(512, activation='sigmoid'))
+model.add(Dense(len(lb.classes_), activation='softmax'))
+
+# inititalize our initial learning rate and # of epochs to train for
+INIT_LR = 0.01
+EPOCHS = 80
+
+# complete the model using SGD as our optimizer and categorical
+# cross-entropy loss (you'll want to use binary_crossentropy
+# for 2-class classification)
+print("[INFO] training network...")
+opt = SGD(lr=INIT_LR)
+model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
+
+# train the neural network
+H = model.fit(x=trainX, y=trainY, validation_data=(testX, testY),
+              epochs=EPOCHS, batch_size=32)
+
+# evaluate the network
+print('[INFO] evaluating network...')
+predictions = model.predict(x=testX, batch_size=32)
+print(classification_report(testY.argmax(axis=1),
+                            predictions.argmax(axis=1, target_names=lb.classes_)))
+
+# plot the training loss and accuracy
+N = np.arange(0, EPOCHS)
+plt.style.use('ggplot')
+plt.figure()
+plt.plot(N, H.history['loss'], label='train_loss')
+plt.plot(N, H.history['val_loss'], label='val_loss')
+plt.plot(N, H.history['accuracy'], label='train_acc')
+plt.plot(N, H.history['val_accuracy'], label='val_lacc')
+plt.title("Training Loss and Accuracy (Simple NN)")
+plt.xlabel('Epoch #')
+plt.ylabel('Loss/Accuracy')
+plt.legend()
+plt.savefig(args['plot'])
 
