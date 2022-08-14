ea5abe052d2adc812c31eb071ab8393832bca2a7
rtmagar <rahulthapa123@outlook.com>
2022-08-14 12:34:30 -0400

    Learn_flip_crop

---
+++ cv-course/Cropping/crop.py
@@ -0,0 +1,8 @@
+# import the necessary packages
+import cv2
+
+# load the image and show it
+image = cv2.imread("florida_trip.png")
+cv2.imshow("Original", image)
+
+cropping an ima
--- cv-course/Flipping/flipping.py
+++ cv-course/Flipping/flipping.py
@@ -1,5 +1,6 @@
 import argparse
 import cv2
+import imutils
 
 ap = argparse.ArgumentParser()
 ap.add_argument('-i', '--image', required=True, help="path to the image")
@@ -20,6 +21,10 @@ cv2.imshow("Flipped Horizontally", flipped)
 flipped = cv2.flip(image, -1)
 cv2.imshow("Flipped Horizontally & Vertically", flipped)
 cv2.waitKey(0)
+
+
+
+
 
 
 
