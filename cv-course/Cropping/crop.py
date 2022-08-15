e634fcb5eabd90c833e74958e613594a618f17ff
rtmagar <rahulthapa123@outlook.com>
2022-08-15 13:05:40 -0400

    crop & image airthmetic

--- cv-course/Cropping/crop.py
+++ cv-course/Cropping/crop.py
@@ -5,4 +5,13 @@ import cv2
 image = cv2.imread("florida_trip.png")
 cv2.imshow("Original", image)
 
-cropping an ima
+# cropping an image is accomplished using simple NumPy array slices --
+# let's crop the face from the image
+face = image[85:250, 85:220]
+cv2.imshow("Face", face)
+cv2.waitKey(0)
+
+# ...and now let's crop the entire body
+body = image[90:450, 0:290]
+cv2.imshow("Body", body)
+cv2.waitKey(0)
